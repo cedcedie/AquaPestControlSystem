1c0596725b7afb7ff14e36d16a5e444763f0e8f9
ENDUSER <ENDUSER@DESKTOP-9I2TKNK>
2024-12-10 15:25:15 +0800

    ced

--- AquaPestControlSystem/Program.cs
+++ AquaPestControlSystem/Program.cs
@@ -28,6 +28,6 @@ app.UseRouting();
 app.UseAuthorization(); // Requires AddAuthorization() in the services
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=User}/{action=UserLogin}/{id?}");
+    pattern: "{controller=User}/{action=LandingPage}/{id?}");
 
 app.Run();
