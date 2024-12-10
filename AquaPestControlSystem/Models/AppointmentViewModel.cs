02ed0c37bf11929c6d28b1295b37ff9c7f1f05ba
Airone Gamil <aironegamil123@gmail.com>
2024-12-10 14:32:30 +0800

    test

---
+++ AquaPestControlSystem/Models/AppointmentViewModel.cs
@@ -0,0 +1,49 @@
+﻿using AquaPestControlSystem.Models.DBEntities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace AquaPestControlSystem.Models
+{
+    public class AppointmentViewModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string AppointmentId { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Middle Name")]
+        public string MiddleName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Contact Number")]
+        public string ContactNum { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("Address")]
+        public string Address { get; set; }
+
+        [DisplayName("Pest Problem")]
+        public string PestProblem { get; set; }
+
+        [DisplayName("Schedule")]
+        public DateTime Schedule { get; set; }
+
+        [DisplayName("Problem Image")]
+        public byte[] ProblemImage { get; set; }
+
+        public string CustomerId { get; set; }
+
+        [DisplayName("Full Name")]
+        public string FullName
+        {
+            get { return FirstName + " " + MiddleName + " " + LastName; }
+        }
+    }
+}
---
+++ AquaPestControlSystem/Models/DBEntities/Appointment.cs
@@ -0,0 +1,40 @@
+﻿using AquaPestControlSystem.DAL;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AquaPestControlSystem.Models.DBEntities
+{
+    public class Appointment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string AppointmentId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ContactNum { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string PestProblem {  get; set; }
+
+        public DateTime Schedule {  get; set; }
+
+        public byte[] ProblemImage { get; set; }
+
+        public string CustomerId { get; set; }
+
+        public Customer customer { get; set; }
+
+        public string FullName
+        {
+            get { return FirstName + " " + MiddleName + " " + LastName;}
+        }
+    }
+}
